f33fc07bc22bde5d9720d4a10d2a84f9721e72d6
Hoa Hoang <HoangTienHoa@users.noreply.github.com>
2023-09-18 11:04:07 +0700

    test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../index");
 describe("GET /", () => {
-    it("respond with Hello World CI", (done) => {
+    it("respond with Hello World", (done) => {
         request(app).get("/").expect("Hello World", done);
     })
 });
