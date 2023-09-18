11f1d043e34393168060638219895abba8d1c11a
Hoa Hoang <HoangTienHoa@users.noreply.github.com>
2023-09-18 11:30:50 +0700

    update function /

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../index");
 describe("GET /", () => {
-    it("respond with Hello World", (done) => {
-        request(app).get("/").expect("Hello World", done);
+    it("respond with Hello World Github Action + Heroku", (done) => {
+        request(app).get("/").expect("Hello World Github Action + Heroku", done);
     })
 });
